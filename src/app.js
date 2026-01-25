7f09eee0b0b74cf411fca2d0c2e7f314c25dca50
Anurag Yadav <jaysinghanurag321@gmail.com>
2026-01-25 12:27:00 +0530

    Middlewares

--- src/app.js
+++ src/app.js
@@ -3,21 +3,21 @@ const PORT=3000;
 
 const app=express();
 
-app.get(/^\/us(e)?r$/,(req,res,next)=>{
-    console.log("HI there")
-     next();
-     console.log("Yeah Baby again !!!!")
-    //res.send({message:"get User Data"});
+// app.get(/^\/us(e)?r$/,(req,res,next)=>{
+//     console.log("HI there")
+//      next();
+//      console.log("Yeah Baby again !!!!")
+//     //res.send({message:"get User Data"});
  
-},(req,res,next)=>{
-    console.log("Second")
-    next();
-    console.log("Yeah Baby");
-    //res.send({message:"get User Data From second Route Handling"});
+// },(req,res,next)=>{
+//     console.log("Second")
+//     next();
+//     console.log("Yeah Baby");
+//     //res.send({message:"get User Data From second Route Handling"});
 
-},(req,res)=>{
-    res.send({message:"This is Third level Bro"})
-})
+// },(req,res)=>{
+//     res.send({message:"This is Third level Bro"})
+// })
 
 
 // app.post("/user/post",(req,res)=>{
@@ -26,6 +26,25 @@ app.get(/^\/us(e)?r$/,(req,res,next)=>{
 // app.use("/get",(req,res)=>{
 //     res.send({message:"Hello From Server"})
 // })
+
+//Its Also Work Like this We can define Like this 
+
+app.use("/",(req,res,next)=>{
+    next();
+    console.log("Waps mai a Gaya !!!")
+    //res.send("Hello Bro");
+})
+app.get("/user",(req,res,next)=>{
+    console.log("First Route");
+    next();
+})
+
+app.get("/user",(req,res,next)=>{
+    console.log("Seconnd Route");
+    res.send({message:"2nd Route Handler"});
+    next();//there is No route After This is returning back So its Not do anythings
+})
+
 
 
 app.listen(PORT ,()=>{
