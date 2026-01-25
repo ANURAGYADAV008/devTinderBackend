cc3edc6bf2cd538a8ab122a9f32d37ab590a4715
Anurag Yadav <jaysinghanurag321@gmail.com>
2026-01-25 12:03:43 +0530

    Route Handler

--- src/app.js
+++ src/app.js
@@ -3,10 +3,31 @@ const PORT=3000;
 
 const app=express();
 
-app.use("/get",(req,res)=>{
-    res.send({message:"Hello From Server"})
+app.get(/^\/us(e)?r$/,(req,res,next)=>{
+    console.log("HI there")
+     next();
+     console.log("Yeah Baby again !!!!")
+    //res.send({message:"get User Data"});
+ 
+},(req,res,next)=>{
+    console.log("Second")
+    next();
+    console.log("Yeah Baby");
+    //res.send({message:"get User Data From second Route Handling"});
+
+},(req,res)=>{
+    res.send({message:"This is Third level Bro"})
 })
 
+
+// app.post("/user/post",(req,res)=>{
+//     res.send({message:"Save User Data"})
+// })
+// app.use("/get",(req,res)=>{
+//     res.send({message:"Hello From Server"})
+// })
+
+
 app.listen(PORT ,()=>{
-    console.log(`server Start Listen at ${PORT}`)
+    console.log(`server Start Listen at ${PORT} PORT`)
 })
